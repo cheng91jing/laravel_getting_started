6f402acf7870af46cb0721fb56c8982b8b37a88c
Chen Jing <654807719@qq.com>
2017-12-06 08:07:36 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
@@ -34,7 +35,19 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|string|max:50',
+            'email' => 'required|string|email|unique:users|max:255',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success', '欢迎，您将会在这里开启一段新的旅程~');
+        return redirect()->route('users.show', ['user' => $user]);
     }
 
     /**
@@ -43,9 +56,9 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
+        return view('users.show', compact('user'));
     }
 
     /**
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,14 +2,17 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title', null) {{ config('app.name', 'Laravel') }}</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
     </head>
     <body>
         @include('layouts._header')
 
         <div class="container">
             <div class="col-md-offset-1 col-md-10">
+                @include('shared._messages')
                 @yield('content')
                 @include('layouts._footer')
             </div>
